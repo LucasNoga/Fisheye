2e39a9c1e5c276d980eea5471dd0eb9108a14b33
Lucas Noga <luc4snoga@gmail.com>
2021-11-27 16:32:34 +0100

    Finalize fisheye dock

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const TRANSFORM_ORIGINS = {
+  RIGHT: "right",
+  CENTER: "center",
+  LEFT: "left"
+};
+
 class Dock {
+  scale = 0.5;
+
   /**
    * @property {HTMLElement} root
    * @property {HTMLElement[]} icons
@@ -14,19 +22,74 @@ class Dock {
     this.iconSize = this.icons[0].offsetWidth;
 
     el.addEventListener("mousemove", this.handleMouseMove.bind(this));
+    el.addEventListener("mouseleave", this.handleMouseLeave.bind(this));
+    el.addEventListener("mouseenter", this.handleMouseEnter.bind(this));
   }
 
-  /**
-   *
-   * @param {MouseEvent} e
-   */
   handleMouseMove(e) {
     this.mousePosition = between(
       (e.clientX - this.root.offsetLeft) / this.iconSize,
       0,
       this.icons.length
     );
-    console.log("MousePosition", this.mousePosition);
+    this.scaleIcons();
+  }
+
+  handleMouseLeave() {
+    this.root.classList.add("animated");
+    this.icons.forEach(icon => {
+      icon.style.removeProperty("transform");
+      icon.style.removeProperty("transform-origin");
+    });
+  }
+
+  handleMouseEnter() {
+    this.root.classList.add("animated");
+    window.setTimeout(() => {
+      this.root.classList.remove("animated");
+    }, 100);
+  }
+
+  /**
+   * Apply transformation css on icons
+   */
+  scaleIcons() {
+    const selectedIndex = Math.floor(this.mousePosition);
+    const centerOffset = this.mousePosition - selectedIndex - 0.5;
+
+    let baseOffset = this.scaleFromDirection(
+      selectedIndex,
+      TRANSFORM_ORIGINS.CENTER,
+      -centerOffset * this.iconSize
+    );
+    let offset = baseOffset * (0.5 - centerOffset);
+    // let offset = 32;
+    for (let i = selectedIndex + 1; i < this.icons.length; i++) {
+      offset += this.scaleFromDirection(i, TRANSFORM_ORIGINS.LEFT, offset);
+    }
+    offset = baseOffset * (0.5 + centerOffset);
+    for (let i = selectedIndex - 1; i >= 0; i--) {
+      offset += this.scaleFromDirection(i, TRANSFORM_ORIGINS.RIGHT, -offset);
+    }
+  }
+
+  /**
+   *
+   * @param {number} index ico
+   * @param {number} direction icon position from center (0: center, -1: left, 1: right)
+   * @param {number} offset
+   */
+  scaleFromDirection(index, direction, offset) {
+    const center = index + 0.5;
+    const distanceFromPointer = this.mousePosition - center;
+    const scale = scaling(distanceFromPointer) * this.scale;
+    const icon = this.icons[index];
+    icon.style.setProperty(
+      "transform",
+      `translateX(${offset}px) scale(${scale + 1})`
+    );
+    icon.style.setProperty("transform-origin", `${direction} bottom`);
+    return scale * this.iconSize;
   }
 }
 
@@ -39,6 +102,14 @@ class Dock {
  */
 function between(val, min, max) {
   return Math.max(min, Math.min(val, max));
+}
+
+/**
+ * Handle scaling depend of distance
+ * @param {number} d
+ */
+function scaling(d) {
+  return Math.max(Math.min(-0.2 * Math.pow(d, 2) + 1.05, 1), 0);
 }
 
 new Dock(document.querySelector(".dock"));
